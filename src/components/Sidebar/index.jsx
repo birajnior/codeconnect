9dc2441965e08e9fb9a1521bd2f3767bdb36af88
birajnior <ubiratanbernardo@gmail.com>
2024-08-28 00:20:38 -0300

    First commit

---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,46 @@
+import Logo from "./assets/Logo.svg";
+import Feed from "./assets/feed.svg";
+import Account from "./assets/account_circle.svg";
+import About_Us from "./assets/info.svg";
+import Logout from "./assets/logout.svg";
+import "./styles.css";
+
+export default function Sidebar() {
+  return (
+    <aside>
+      <img src={Logo} alt="Logo CodeConnect" />
+      <nav>
+        <ul className="lista-sidebar">
+          <li>
+            <a href="#" className="item__link-piblicacao">Publicar</a>
+            </li>
+            <li>
+              <a href="#" className="item__link item__link--ativo">
+                <img src={Feed} />
+                <span>Feed</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="item__link">
+                <img src={Account} />
+                <span>Perfil</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="item__link">
+                <img src={About_Us} />
+                <span>Sobre nós</span>
+              </a>
+            </li>
+            <li>
+              <a href="#" className="item__link">
+                <img src={Logout} />
+                <span>Sair</span>
+              </a>
+            </li>
+          
+        </ul>
+      </nav>
+    </aside>
+  );
+}

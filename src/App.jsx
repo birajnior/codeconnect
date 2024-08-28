e514e355b07f658e7d14a64117399a05128950c7
birajnior <ubiratanbernardo@gmail.com>
2024-08-28 18:28:28 -0300

    Componentes de Barra de pesquisa e Filtro

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
-import './App.css'
-import Sidebar from './components/Sidebar'
+import "./App.css";
+import BarraDePesquisa from "./components/BarraDePesquisa";
+import Filtro from "./components/Filtro";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Sidebar />
-    </>
-  )
+      <div>
+        <BarraDePesquisa />
+        <Filtro />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
